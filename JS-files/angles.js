448041dcd738fbf9aba2d5d5deb6f0297e618b91
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-08-15 12:29:20 +0530

    add settime

--- JS-files/angles.js
+++ JS-files/angles.js
@@ -15,13 +15,13 @@ form.addEventListener("submit",(e)=>{
     angle_two=Number(angle_two.value);
     angle_three=Number(angle_three.value);
     let sum_of_three=angle_one+angle_two+angle_three;
-
-    if(angle_one===0 || angle_two ===0|| angle_three ===0){
-        output_div.innerHTML="Please note than an each angle should be greater than 0'";
-        
-    }else if(sum_of_three<180 || sum_of_three > 180){
+    if(sum_of_three<180 || sum_of_three > 180){
         output_div.innerHTML="OOPS! These angles cannot form triangle, Note that the sum of 3 angles should be equal 180 degrees.";
+        setTimeout(()=>{
+            output_div.style.display='none'
+        },2500)
     }else{
+        output_div.style.display='block'
         output_div.innerHTML="YAY! We can form a triangle using these."
     }
     e.preventDefault()
